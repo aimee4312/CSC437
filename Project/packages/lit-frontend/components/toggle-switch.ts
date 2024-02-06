b688f2d7102552124b01636816fc53664fcf108f
Aimee <aimee4312@gmail.com>
2024-02-06 13:46:35 -0800

    implemented the toggle switch

--- Project/packages/lit-frontend/components/toggle-switch.ts
+++ Project/packages/lit-frontend/components/toggle-switch.ts
@@ -10,7 +10,7 @@ class ToggleSwitchElement extends LitElement {
     return html`<label>
       <slot>Label</slot>
       <span class="slider">
-        <input type="checkbox" @change=${this._handleChange} />
+        <input type="checkbox" @change=${this._handleChange} checked/>
       </span>
     </label>`;
   }
@@ -21,34 +21,35 @@ class ToggleSwitchElement extends LitElement {
     }
     label {
       display: flex;
+      justify-content: flex-end;
       align-items: center;
-      gap: var(--size-spacing-medium);
       line-height: 2em;
     }
     .slider {
       display: inline-block;
-      border: 1px solid var(--color-slider-border);
+      border: 1px solid var(--color-slider-checked);
       border-radius: 0.75em;
-      background-color: var(--color-light-background);
+      background-color: var(--color-slider-background);
+      margin: 1em;
       height: 1.5em;
       width: 2.75em;
       position: relative;
       transition: background-color
-      var(--color-slider-transition);
+      var(--time-transition-control);
     }
     .slider:has(input:checked) {
-      background-color: var(--color-slider-border);
+      background-color: var(--color-slider-checked);
     }
     input {
       appearance: none;
-      background-color: var(--color-slider-button);
+      background-color: var(--color-slider-forground);
       border-radius: 50%;
       width: 1.25em;
       height: 1.25em;
       vertical-align: center;
       position: absolute;
       left: 0;
-      transition: left var(--color-slider-transition);
+      transition: left var(--time-slider-transition);
     }
     input:checked {
       left: 1.5em;
